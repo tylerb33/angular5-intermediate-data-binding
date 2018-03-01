6445a3c44e398cbe4d9d8f6590e658d66bd26b6e
Tyler Barnett <tyler.barnett7190@gmail.com>
2018-03-01 17:20:51 -0600

    now able to pass data between classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'Testserver', content: 'This is only a test.'}];
 
 
 
